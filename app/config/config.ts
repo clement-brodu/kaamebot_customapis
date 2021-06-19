ebdad84f174c52c68ffbebd23e08b391b70ba1f3
Clément Brodu <clement.brodu@gmail.com>
2021-06-19 12:20:24 +0200

    fix: port

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,11 +1,12 @@
 import { Config } from '../types';
 
 let env = process.env.NODE_ENV || 'development';
+let vport = process.env.PORT || '8081';
 
 export let settings: Config = {
   name: 'kaamebot_customapis',
   version: '1.0.0',
-  port: process.env.PORT || 8081,
+  port: Number(vport),
   env: 'dev'
 };
 
